a7b740b37e3daeb87c7aba4ef026a6b05bbe8f73
Aaron Santana <aaron.satana@ucr.ac.cr>
2023-06-29 11:37:17 -0600

    fixing person not found

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
 
-const scrapeLogic = async (res,cedula) => {
+const scrapeLogic = async (res, cedula) => {
   const browser = await puppeteer.launch({
     args: [
       "--disable-setuid-sandbox",
@@ -16,36 +16,48 @@ const scrapeLogic = async (res,cedula) => {
   });
   try {
     const page = await browser.newPage();
-    await page.goto('https://servicioselectorales.tse.go.cr/chc/consulta_cedula.aspx');
-    await page.type('#txtcedula', cedula)
-    await page.click('#btnConsultaCedula');
-    await page.waitForSelector('#LinkButton11')
-    await page.click('#LinkButton11')
-    await page.waitForSelector('#lblnombre')
+    await page.goto(
+      "https://servicioselectorales.tse.go.cr/chc/consulta_cedula.aspx"
+    );
+    await page.type("#txtcedula", cedula);
+    await page.click("#btnConsultaCedula");
+
+
   
-     // cedula,nombre, padre, cedulaPadre, madre, cedulaMadre, fechaNacimiento, nacionalidad, edad, marginalidad
-  
-    const info = await page.evaluate(() =>{
-      return  {
-        cedula: document.getElementById('lblcedula').textContent,
-        nombre: document.getElementById('lblnombre').textContent,
-        primerApellido: document.getElementById('lblprimer_apellido').textContent,
-        segundoApellido: document.getElementById('lblsegundo_apellido').textContent,
-        padre: {
-          nombre: document.getElementById('lblnombre_padre').textContent,
-          cedula: document.getElementById('lblid_padre').textContent
-        },
-        madre: {
-          nombre: document.getElementById('lblnombre_madre').textContent,
-          cedula: document.getElementById('lblid_madre').textContent
-        },
-        fechaNacimiento: document.getElementById('lblfecha_nacimiento').textContent,
-        nacionalidad: document.getElementById('lblnacionalidad').textContent,
-        marginalidad: document.getElementById('lblLeyendaMarginal').textContent
-        
-      } 
-    })
-    
+    if (await page.$("#Button1") != null) {
+      await page.waitForSelector("#LinkButton11");
+      await page.click("#LinkButton11");
+      await page.waitForSelector("#lblnombre");
+    } else {
+      res.send('Persona no encontrada')
+      return
+    }
+    // cedula,nombre, padre, cedulaPadre, madre, cedulaMadre, fechaNacimiento, nacionalidad, edad, marginalidad
+
+    const info = await page.evaluate( async () => {
+        return {
+          cedula: document.getElementById("lblcedula").textContent,
+          nombre: document.getElementById("lblnombre").textContent,
+          primerApellido:
+            document.getElementById("lblprimer_apellido").textContent,
+          segundoApellido: document.getElementById("lblsegundo_apellido")
+            .textContent,
+          padre: {
+            nombre: document.getElementById("lblnombre_padre").textContent,
+            cedula: document.getElementById("lblid_padre").textContent,
+          },
+          madre: {
+            nombre: document.getElementById("lblnombre_madre").textContent,
+            cedula: document.getElementById("lblid_madre").textContent,
+          },
+          fechaNacimiento: document.getElementById("lblfecha_nacimiento")
+            .textContent,
+          nacionalidad: document.getElementById("lblnacionalidad").textContent,
+          marginalidad:
+            document.getElementById("lblLeyendaMarginal").textContent,
+        };
+    });
+
     res.send(info);
   } catch (e) {
     console.error(e);
