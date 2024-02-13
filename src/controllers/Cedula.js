9e62231d586217ee84a7386a89d58ccb1da1daf8
Aaron Santana <system@savaldev.com>
2024-02-13 05:26:31 +0000

    feat: invalid cedula handler

--- src/controllers/Cedula.js
+++ src/controllers/Cedula.js
@@ -3,6 +3,7 @@ const CedulaModel = require('../model/Cedula');
 class CedulaController {
   static async get(req, res) {
     const { cedula } = req.params;
+    if (!cedula || cedula.length !== 9) throw new Error('Cedula no válida | 400');
     const info = await CedulaModel.get(cedula);
     res.send(info);
   }
