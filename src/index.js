e7b208d06aee40eb700ecb2519f56a1f22ad5c06
Aaron Santana <system@savaldev.com>
2024-02-13 05:11:57 +0000

    feat: rendering docs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
 const cors = require('cors')
 const app = express();
 const morgan = require('morgan')
+const patt = require('path')
 
 app.use(cors({
   origin: '*'
@@ -12,8 +12,12 @@ app.use(morgan('dev'))
 
 const PORT = process.env.PORT || 7504;
 
+app.use(express.static(patt.join(__dirname, 'docs')));
+
+app.get('/', (req, res) => {
+  res.sendFile (patt.join(__dirname, 'docs', 'index.html'))
+});
 /* ---------- V1 ---------- */
-app.use(require("./routes/v1/home"));
 app.use(require("./routes/v1/cedula"));
 
 /* ---------- V2 ---------- */
